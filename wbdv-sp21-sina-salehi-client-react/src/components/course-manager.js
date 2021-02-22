1f6f7212a563a86487336d5534bb24de0c572761
salehi-s <salehi.s@husky.neu.edu>
2021-02-22 13:41:11 -0500

    Create course-editor.js and paste in course editor body from previous assignment

--- wbdv-sp21-sina-salehi-client-react/src/components/course-manager.js
+++ wbdv-sp21-sina-salehi-client-react/src/components/course-manager.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import CourseTable from "./course-table";
+import CourseGrid from "./course-grid";
 
 const CourseManager = () =>
     <div>
         <h1>Course Manager</h1>
         <CourseTable/>
+        <CourseGrid/>
     </div>
 
 export default CourseManager
