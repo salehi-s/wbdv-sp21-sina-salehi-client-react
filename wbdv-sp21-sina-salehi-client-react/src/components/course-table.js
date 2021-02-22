dfe35e839aca0a8c5adb12f30f7e1e064309fa9d
salehi-s <salehi.s@husky.neu.edu>
2021-02-22 13:59:49 -0500

    Populate CourseTable with dummy data and create CourseRow (init)

--- wbdv-sp21-sina-salehi-client-react/src/components/course-table.js
+++ wbdv-sp21-sina-salehi-client-react/src/components/course-table.js
@@ -4,7 +4,35 @@ export default class CourseTable
     extends React.Component {
     render() {
         return(
-            <h2>Course Table</h2>
+            <div>
+                <h2>Course Table</h2>
+                <table className = "table">
+                    <tbody>
+                        <tr>
+                            <td>CS5610 (Web Development)</td>
+                            <td>Me</td>
+                            <td>January 1, 2021</td>
+                            <td>1:00 P.M.</td>
+                            <td>
+                                <i className = "fas fa-check"></i>
+                                <i className = "fas fa-trash"></i>
+                                <i className = "fas fa-edit"></i>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>DS5110 (Introduction to Data Management and Processing)</td>
+                            <td>Me</td>
+                            <td>January 14, 2021</td>
+                            <td>7:00 P.M.</td>
+                            <td>
+                                <i className = "fas fa-check"></i>
+                                <i className = "fas fa-trash"></i>
+                                <i className = "fas fa-edit"></i>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         )
     }
 }
