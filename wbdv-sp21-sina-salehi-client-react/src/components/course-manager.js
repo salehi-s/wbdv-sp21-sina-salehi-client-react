6470e843a58aff8ea59880ba909f27ab0f0a4115
salehi-s <salehi.s@husky.neu.edu>
2021-02-22 13:25:08 -0500

    Create course-table.js and course-grid.js (init)

--- wbdv-sp21-sina-salehi-client-react/src/components/course-manager.js
+++ wbdv-sp21-sina-salehi-client-react/src/components/course-manager.js
@@ -1,6 +1,10 @@
 import React from 'react'
+import CourseTable from "./course-table";
 
 const CourseManager = () =>
-    <h1>Course Manager</h1>
+    <div>
+        <h1>Course Manager</h1>
+        <CourseTable/>
+    </div>
 
 export default CourseManager
---
+++ wbdv-sp21-sina-salehi-client-react/src/components/course-table.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default class CourseTable
+    extends React.Component {
+    render() {
+        return(
+            <h2>Course Table</h2>
+        )
+    }
+}
