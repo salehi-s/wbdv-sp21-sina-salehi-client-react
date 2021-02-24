37b15bf09477714ab85781a40c0dc9046c87a568
salehi-s <salehi.s@husky.neu.edu>
2021-02-24 17:09:04 -0500

    Add placeholder image to Course Card

--- wbdv-sp21-sina-salehi-client-react/src/components/course-grid/course-card.js
+++ wbdv-sp21-sina-salehi-client-react/src/components/course-grid/course-card.js
@@ -6,10 +6,12 @@ const CourseCard = ({course}) =>
         <div className = "card">
             <div className = "card-body">
                 <h5 className = "card-title">{course.title}</h5>
-                <p className = "card-text">Some quick example text to build on the card title and
-                    make up the bulk of the card's content.</p>
+                <img src = "Book.svg" className = "card-img-top" alt = "..."/>
+                <p className = "card-text">Owned By: {course.owner}</p>
+                <p className = "card-text">Date Last Modified: {course.lastModified}</p>
+                <p className = "card-text">Time Last Modified: {course.time}</p>
                 <Link to = "/courses/editor" className = "btn btn-primary">
-                    {course.title}
+                    Go to {course.title}
                 </Link>
                 <i className = "fas fa-trash"></i>
             </div>
